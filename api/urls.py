41245e59e46d2d54d43c94f11241198c09d15d31
Abdirayimov <orifjon12032000@gmail.com>
2022-01-10 15:20:38 +0500

    created task-create,update views

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
 	path('', views.apiOverView, name='api-overview'),
 	path('task-list/', views.taskList, name='task-list'),
 	path('task-list/<str:pk>/', views.taskDetail, name='task-detail'),
+	path('task-create/', views.taskCreate, name='task-create'),
+	path('task-update/<str:pk>', views.taskUpdate, name='task-update'),
 ]
+
 
